ba14012c9cc30a54552c0372c66d12f2e0a0d412
Zachary Gelfond <zmg24@cornell.edu>
2022-01-15 20:50:05 -0500

    Added project

---
+++ mysolanaapp/app/src/App.js
@@ -0,0 +1,179 @@
+import './App.css';
+import { useState } from 'react';
+import { Connection, PublicKey /*, clusterApiUrl*/ } from '@solana/web3.js';
+import { Program, Provider, web3 } from '@project-serum/anchor';
+import idl from './idl.json';
+
+import { getPhantomWallet } from '@solana/wallet-adapter-wallets';
+import { useWallet, WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+const wallets = [getPhantomWallet()]
+
+const { SystemProgram, Keypair } = web3;
+const baseAccount = Keypair.generate();
+const opts = {
+  preflightCommitment: "processed"
+}
+const programID = new PublicKey(idl.metadata.address);
+
+function App() {
+  const [value, setValue] = useState('');
+  const [dataList, setDataList] = useState([]);
+  const [input, setInput] = useState('');
+  const [input2, setInput2] = useState('');
+  const wallet = useWallet();
+  const [start, setStart] = useState(false);
+  const [number, setNumber] = useState('');
+
+  async function getProvider() {
+    /* create the provider and return it to the caller */
+    /* network set to local network for now */
+    const network = "http://127.0.0.1:8899";
+    const connection = new Connection(network, opts.preflightCommitment);
+
+    const provider = new Provider(
+      connection, wallet, opts.preflightCommitment,
+    );
+    return provider;
+  }
+
+  async function initialize() {
+    const provider = await getProvider();
+    /* create the program interface combining the idl, program ID, and provider */
+    const program = new Program(idl, programID, provider);
+    try {
+      /* interact with the program via rpc */
+      await program.rpc.initialize("Hello World", {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [baseAccount]
+      });
+
+      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+      console.log('account: ', account);
+      setValue(account.data.toString());
+      setDataList(account.dataList);
+      setNumber(account.number.toString())
+      setStart(true);
+    } catch (err) {
+      console.log("Transaction error: ", err);
+    }
+  }
+
+  async function update() {
+    if (!input) return
+    const provider = await getProvider();
+    const program = new Program(idl, programID, provider);
+    await program.rpc.update(input, {
+      accounts: {
+        baseAccount: baseAccount.publicKey
+      }
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('account: ', account);
+    setValue(account.data.toString());
+    setDataList(account.dataList);
+    setNumber(account.number.toString())
+    setInput('');
+  }
+
+  async function remove() {
+    const provider = await getProvider();
+    const program = new Program(idl, programID, provider);
+    await program.rpc.remove({
+      accounts: {
+        baseAccount: baseAccount.publicKey
+      }
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('account: ', account);
+    setValue(account.data.toString());
+    setDataList(account.dataList);
+    setNumber(account.number.toString())
+    setInput('');
+  }
+
+  async function SetNumber() {
+    if (!input2) return
+    const provider = await getProvider();
+    const program = new Program(idl, programID, provider);
+    await program.rpc.setnum(input2, {
+      accounts: {
+        baseAccount: baseAccount.publicKey
+      }
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('account: ', account);
+    setValue(account.data.toString());
+    setDataList(account.dataList);
+    setNumber(account.number.toString())
+    setInput('');
+  }
+
+  if (!wallet.connected) {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '100px' }}>
+        <WalletMultiButton />
+      </div>
+    )
+  } else {
+    return (
+      <div className="App">
+        <div>
+          {
+            !start && (<button onClick={initialize}>Initialize</button>)
+          }
+
+          {
+            start ? (
+              <div>
+                <h2>Current value: {value} and current number: {number} </h2>
+                <input
+                  placeholder="Add new data"
+                  onChange={e => setInput(e.target.value)}
+                  value={input}
+                />
+                <button onClick={update}>Add data</button>
+                <button onClick={remove}>Remove data</button>
+                <input
+                  placeholder="Add new number"
+                  onChange={e => setInput2(e.target.value)}
+                  number={input2}
+                />
+                <button onClick={SetNumber}>Set Number</button>
+              </div>
+            ) : (
+              <h3>Please Initialize.</h3>
+            )
+          }
+          {
+            dataList.map((d, i) => <h4 key={i}>{d}</h4>)
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+// const network = clusterApiUrl('devnet');
+
+
+const AppWithProvider = () => (
+  <ConnectionProvider endpoint="http://127.0.0.1:8899">
+    <WalletProvider wallets={wallets} autoConnect>
+      <WalletModalProvider>
+        <App />
+      </WalletModalProvider>
+    </WalletProvider>
+  </ConnectionProvider>
+)
+
+export default AppWithProvider; 
---
+++ mysolanaapp/tests/mysolanaapp.js
@@ -0,0 +1,76 @@
+const assert = require("assert");
+const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
+
+describe("Mysolanaapp", () => {
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.Mysolanaapp;
+  it("It initializes the account", async () => {
+    const baseAccount = anchor.web3.Keypair.generate();
+    await program.rpc.initialize("Hello World", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Data: ', account.data);
+    assert.ok(account.data === "Hello World");
+    _baseAccount = baseAccount;
+
+  });
+
+  it("Updates a previously created account", async () => {
+    const baseAccount = _baseAccount;
+
+    await program.rpc.update("Some new data", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Updated data: ', account.data)
+    assert.ok(account.data === "Some new data");
+    console.log('all account data:', account)
+    console.log('All data: ', account.dataList);
+    assert.ok(account.dataList.length === 2);
+  });
+
+  it("Removes from a previously created account", async () => {
+    const baseAccount = _baseAccount;
+
+    await program.rpc.remove({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Updated data: ', account.data)
+    assert.ok(account.data === "Hello World");
+    console.log('all account data:', account)
+    console.log('All data: ', account.dataList);
+    assert.ok(account.dataList.length === 1);
+  });
+
+  it("Set number", async () => {
+    const baseAccount = _baseAccount;
+    await program.rpc.setnum("7", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Updated number: ', account.number)
+    assert.ok(account.number.toString() == 7);
+    console.log('all account data:', account)
+    console.log('All data: ', account.dataList);
+    assert.ok(account.dataList.length === 1);
+  });
+});
